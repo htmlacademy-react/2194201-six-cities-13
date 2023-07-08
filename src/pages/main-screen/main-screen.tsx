87c541f01b2814e41e5631e453e431b45bb40685
NeXan <nexan87@gmail.com>
2023-07-08 11:45:26 +0300

    Добавляет компонент SortForm

--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -1,5 +1,6 @@
 import Header from '../../components/header/header';
 import Tabs from '../../components/tabs/tabs';
+import SortForm from '../../components/sort-form/sort-form';
 import PlaceCard from '../../components/place-card/place-card';
 import { CardList } from '../../components/consts';
 
@@ -15,32 +16,7 @@ function MainScreen(): JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">312 places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
+              <SortForm />
               <div className="cities__places-list places__list tabs__content">
                 {CardList.map((card) => (
                   <PlaceCard card={card} key={Math.random()} />
