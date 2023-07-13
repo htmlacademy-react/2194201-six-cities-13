ce07ad4d0fe6f89d6b34823f4c4a1c782aa112c8
Denis <nexan87@gmail.com>
2023-07-13 20:29:58 +0300

    Удаляет непереиспользуемые в проекте компоненты

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,3 @@
-import Logo from '../logo/logo';
-import UserNav from '../user-nav/user-nav';
-
 type HeaderProps = {
   isUserNav?: boolean;
 };
@@ -11,9 +8,39 @@ function Header({ isUserNav = false }: HeaderProps): JSX.Element {
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <Logo />
+            <a className="header__logo-link">
+              <img
+                className="header__logo"
+                src="img/logo.svg"
+                alt="6 cities logo"
+                width={81}
+                height={41}
+              />
+            </a>
           </div>
-          {isUserNav && <UserNav />}
+          {isUserNav && (
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <a
+                    className="header__nav-link header__nav-link--profile"
+                    href="#"
+                  >
+                    <div className="header__avatar-wrapper user__avatar-wrapper" />
+                    <span className="header__user-name user__name">
+                      Oliver.conner@gmail.com
+                    </span>
+                    <span className="header__favorite-count">3</span>
+                  </a>
+                </li>
+                <li className="header__nav-item">
+                  <a className="header__nav-link" href="#">
+                    <span className="header__signout">Sign out</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          )}
         </div>
       </div>
     </header>
