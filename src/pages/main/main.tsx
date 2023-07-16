1d42ed46e876f53b98180b27673adb9470b9d88f
Denis <nexan87@gmail.com>
2023-07-16 12:19:22 +0300

    Исправляет ошибку с отсутствием title в Main

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async';
 import PlaceCard from '../../components/place-card/place-card';
 import Map from '../../components/map/map';
 import Header from '../../components/header/header';
@@ -16,6 +17,9 @@ function Main({ numberOffers, cardList }: MainProps): JSX.Element {
 
   return (
     <div className="page page--gray page--main">
+      <Helmet>
+        <title>Онлайн-бронирование гостиниц</title>
+      </Helmet>
       <Header isUserNav />
       <main className={`page__main page__main--index ${MainEmptyClass}`}>
         <h1 className="visually-hidden">Cities</h1>
