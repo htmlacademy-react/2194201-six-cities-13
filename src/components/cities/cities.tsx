3f9b2476e798ec78ca8f25a80d30223e51943bb2
NeXan <nexan87@gmail.com>
2023-08-01 12:30:56 +0300

    Создает компонент Places

--- src/components/cities/cities.tsx
+++ src/components/cities/cities.tsx
@@ -1,57 +1,37 @@
 import { useState } from 'react';
-import PlaceCard from '../../components/place-card/place-card';
 import Map from '../../components/map/map';
 import { Card } from '../../types';
 import { getOffersLocation } from '../../helpers/get-offers-location';
-import SortOffers from '../sort-offers/sort-offers';
-import { useAppSelector } from '../../hooks';
-import { selectActiveSort, selectActiveCity } from '../../store/action';
-import { useSortOffers } from '../../hooks/use-sort-offers/use-sort-offers';
+import { MainEmpty } from '../../components/main-empty/main-empty';
+import Places from '../places/places';
 
 type CitiesProps = {
-  offers: Card[];
+  currentOffers: Card[];
 };
 
-function Cities({ offers }: CitiesProps): JSX.Element {
+function Cities({ currentOffers }: CitiesProps): JSX.Element {
   const [cardId, setCardId] = useState<string>('');
-  const activeCity = useAppSelector(selectActiveCity);
-  const activeSort = useAppSelector(selectActiveSort);
-
-  const sortOffers = useSortOffers(activeSort, offers);
-  const offersLocation = getOffersLocation(offers);
-
-  const handlePlaceCardMouseOver = (id: string) => setCardId(id);
-  const handlePlaceCardMouseLeave = () => setCardId('');
+  const offersLocation = getOffersLocation(currentOffers);
+  const isNotEmpty = !!currentOffers.length;
 
   return (
-    <div className="cities__places-container container">
-      <section className="cities__places places">
-        <h2 className="visually-hidden">Places</h2>
-        <b className="places__found">
-          {sortOffers.length} places to stay in {activeCity}
-        </b>
-        <SortOffers />
-        <div className="cities__places-list places__list tabs__content">
-          {sortOffers.map((offer) => (
-            <PlaceCard
-              card={offer}
+    <div className="cities">
+      {isNotEmpty ? (
+        <div className="cities__places-container container">
+          <Places currentOffers={currentOffers} setCardId={setCardId} />
+          <div className="cities__right-section">
+            <Map
               className="cities"
-              key={offer.id}
-              handlePlaceCardMouseOver={handlePlaceCardMouseOver}
-              handlePlaceCardMouseLeave={handlePlaceCardMouseLeave}
+              height="100%"
+              cityInfo={currentOffers[0].city}
+              offers={offersLocation}
+              cardId={cardId}
             />
-          ))}
+          </div>
         </div>
-      </section>
-      <div className="cities__right-section">
-        <Map
-          className="cities"
-          height="100%"
-          cityInfo={offers[0].city}
-          offers={offersLocation}
-          cardId={cardId}
-        />
-      </div>
+      ) : (
+        <MainEmpty />
+      )}
     </div>
   );
 }
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -2,7 +2,6 @@ import cn from 'classnames';
 import { Helmet } from 'react-helmet-async';
 import Header from '../../components/header/header';
 import Tabs from '../../components/tabs/tabs';
-import { MainEmpty } from '../../components/main-empty/main-empty';
 import Cities from '../../components/cities/cities';
 import { useAppSelector } from '../../hooks';
 import { selectActiveCity, getOffers } from '../../store/action';
@@ -26,9 +25,7 @@ function Main(): JSX.Element {
       >
         <h1 className="visually-hidden">Cities</h1>
         <Tabs />
-        <div className="cities">
-          {isNotEmpty ? <Cities offers={currentOffers} /> : <MainEmpty />}
-        </div>
+        <Cities currentOffers={currentOffers} />
       </main>
     </div>
   );
