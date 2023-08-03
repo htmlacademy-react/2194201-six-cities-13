8f5195a148a9bc835080cad70158d2e9bc01f6da
NeXan <nexan87@gmail.com>
2023-08-03 10:28:13 +0300

    Оптимизирует код в компоненте Reviews

--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -27,9 +27,11 @@ function Reviews({ offerId }: ReviewsProps): JSX.Element {
     }
   }, [offerId, dispatch]);
 
-  const reviewList = [...useAppSelector(getOfferReviews)];
+  const reviewList = useAppSelector(getOfferReviews);
 
-  reviewList.sort((a, b) => dayjs(b.date).diff(a.date));
+  const sortedReviews = [...reviewList]
+    .sort((a, b) => dayjs(b.date).diff(a.date))
+    .slice(0, MAX_REVIEWS);
 
   return (
     <section className="offer__reviews reviews">
@@ -37,47 +39,43 @@ function Reviews({ offerId }: ReviewsProps): JSX.Element {
         Reviews · <span className="reviews__amount">{reviewList.length}</span>
       </h2>
       <ul className="reviews__list">
-        {reviewList.map(({ id, date, user, comment, rating }, index) => {
+        {sortedReviews.map(({ id, date, user, comment, rating }) => {
           const reviewDate = dayjs(date).format(DATE);
           const year = dayjs(date).year();
           const month = dayjs(date).format(MONTH_TEXT);
 
-          if (index < MAX_REVIEWS) {
-            return (
-              <li className="reviews__item" key={id}>
-                <div className="reviews__user user">
-                  <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                    <img
-                      className="reviews__avatar user__avatar"
-                      src={user.avatarUrl}
-                      width={54}
-                      height={54}
-                      alt="Reviews avatar"
+          return (
+            <li className="reviews__item" key={id}>
+              <div className="reviews__user user">
+                <div className="reviews__avatar-wrapper user__avatar-wrapper">
+                  <img
+                    className="reviews__avatar user__avatar"
+                    src={user.avatarUrl}
+                    width={54}
+                    height={54}
+                    alt="Reviews avatar"
+                  />
+                </div>
+                <span className="reviews__user-name">{user.name}</span>
+              </div>
+              <div className="reviews__info">
+                <div className="reviews__rating rating">
+                  <div className="reviews__stars rating__stars">
+                    <span
+                      style={{
+                        width: `${rating * ONE_STAR_RATIO}%`,
+                      }}
                     />
+                    <span className="visually-hidden">Rating</span>
                   </div>
-                  <span className="reviews__user-name">{user.name}</span>
                 </div>
-                <div className="reviews__info">
-                  <div className="reviews__rating rating">
-                    <div className="reviews__stars rating__stars">
-                      <span
-                        style={{
-                          width: `${rating * ONE_STAR_RATIO}%`,
-                        }}
-                      />
-                      <span className="visually-hidden">Rating</span>
-                    </div>
-                  </div>
-                  <p className="reviews__text">{comment}</p>
-                  <time className="reviews__time" dateTime={reviewDate}>
-                    {month} {year}
-                  </time>
-                </div>
-              </li>
-            );
-          }
-
-          return null;
+                <p className="reviews__text">{comment}</p>
+                <time className="reviews__time" dateTime={reviewDate}>
+                  {month} {year}
+                </time>
+              </div>
+            </li>
+          );
         })}
       </ul>
       {isAuth && <ReviewForm />}
