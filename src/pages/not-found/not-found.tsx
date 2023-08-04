6ab0683a6eeb7959f0026c16c6f8e19ae59fdc85
Denis <nexan87@gmail.com>
2023-08-04 18:07:57 +0300

    Стилизует страницу 404

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -3,18 +3,22 @@ import { Link } from 'react-router-dom';
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 import { AppRoute } from '../../constants';
+import main from './not-found.module.css';
 
 function NotFound(): JSX.Element {
   return (
-    <div className="page page--gray page--main">
+    <div className="page page--main">
       <Helmet>
         <title>Страница не найдена!</title>
       </Helmet>
       <Header isUserNav />
-      <main className="page__main page__main--index">
+      <main className={`page__main ${main['page__main--not-found']}`}>
         <h1 className="visually-hidden">Ошибка 404. Страница не найдена!</h1>
         <p>Страница не найдена! Попробуйте воспользоваться поиском.</p>
-        <Link to={AppRoute.Root}>Перейти на главную страницу.</Link>
+        <b className={main['page__status']}>404</b>
+        <Link to={AppRoute.Root} className={main['page__link']}>
+          Перейти на главную страницу.
+        </Link>
       </main>
       <Footer />
     </div>
