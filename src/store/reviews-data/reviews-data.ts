61be546e5e3407900646ad6dffd0f4c7884929e1
NeXan <nexan87@gmail.com>
2023-08-08 15:14:29 +0300

    Добавляет глобальное состояние statusReview

--- src/store/reviews-data/reviews-data.ts
+++ src/store/reviews-data/reviews-data.ts
@@ -1,14 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { NameSpace } from '../../constants';
+import { NameSpace, Status } from '../../constants';
 import { fetchReviewsAction, postReviewAction } from '../api-actions';
 import { Review } from '../../types';
 
 type ReviewsData = {
   reviews: Review[];
+  statusReview: string;
 };
 
 const initialState: ReviewsData = {
   reviews: [],
+  statusReview: Status.Unknown,
 };
 
 export const reviewsData = createSlice({
@@ -20,8 +22,15 @@ export const reviewsData = createSlice({
       .addCase(fetchReviewsAction.fulfilled, (state, action) => {
         state.reviews = action.payload;
       })
+      .addCase(postReviewAction.pending, (state) => {
+        state.statusReview = Status.Loading;
+      })
       .addCase(postReviewAction.fulfilled, (state, action) => {
         state.reviews.push(action.payload);
+        state.statusReview = Status.Success;
+      })
+      .addCase(postReviewAction.rejected, (state) => {
+        state.statusReview = Status.Error;
       });
   },
 });
