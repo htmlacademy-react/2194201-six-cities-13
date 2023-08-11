7bb1806530418c634eda84b22b849bdd8fab6052
Denis <nexan87@gmail.com>
2023-08-11 19:10:20 +0300

    Исправляет ошибки в форме комментария

--- src/components/form-review-rating/form-review-rating.tsx
+++ src/components/form-review-rating/form-review-rating.tsx
@@ -1,33 +1,42 @@
 import { memo } from 'react';
+import { useAppSelector } from '../../hooks';
+import { selectStatusPost } from '../../store/reviews-data/selectors';
+import { Status } from '../../constants';
 
 type FormReviewRatingProps = {
   star: number;
   title: string;
+  currentValue: number;
   onInputsChange: (star: number) => void;
 };
 
 const FormReviewRating = memo(
-  ({ star, title, onInputsChange }: FormReviewRatingProps) => (
-    <>
-      <input
-        onChange={() => onInputsChange?.(star)}
-        className="form__rating-input visually-hidden"
-        name="rating"
-        value={star}
-        id={`${star}-star`}
-        type="radio"
-      />
-      <label
-        htmlFor={`${star}-star`}
-        className="reviews__rating-label form__rating-label"
-        title={title}
-      >
-        <svg className="form__star-image" width={37} height={33}>
-          <use xlinkHref="#icon-star" />
-        </svg>
-      </label>
-    </>
-  )
+  ({ star, title, currentValue, onInputsChange }: FormReviewRatingProps) => {
+    const status = useAppSelector(selectStatusPost);
+    return (
+      <>
+        <input
+          onChange={() => onInputsChange?.(star)}
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value={star}
+          id={`${star}-stars`}
+          checked={currentValue === star}
+          type="radio"
+          disabled={status === Status.Loading}
+        />
+        <label
+          htmlFor={`${star}-stars`}
+          className="reviews__rating-label form__rating-label"
+          title={title}
+        >
+          <svg className="form__star-image" width={37} height={33}>
+            <use xlinkHref="#icon-star" />
+          </svg>
+        </label>
+      </>
+    );
+  }
 );
 
 FormReviewRating.displayName = 'FormReviewRating';
