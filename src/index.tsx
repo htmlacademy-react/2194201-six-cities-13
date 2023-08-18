a5e9bfe5afe360338bb6bf7189e4d8030c906da2
NeXan <nexan87@gmail.com>
2023-08-18 14:41:22 +0300

    WIP Делает рефакторинг функции changeOffersFavorite

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,17 @@ import { store } from './store';
 import App from './components/app/app';
 import ErrorMessage from './components/error-message/error-message';
 import { fetchOffersAction, checkAuthAction } from './store/api-actions';
+import { makeFakeOffers } from './utils/mocks/offers';
 
 store.dispatch(fetchOffersAction());
 store.dispatch(checkAuthAction());
+console.log(
+  makeFakeOffers({
+    id: 'adj4ag4k6a4jk6da8',
+    isFavorite: false,
+    isOneIdLiteral: true,
+  })
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- src/utils/mocks/offers.ts
+++ src/utils/mocks/offers.ts
@@ -8,35 +8,42 @@ type OffersParameters = {
   isOneIdLiteral?: boolean;
 };
 
-const makeFakeOffers = (parameters?: OffersParameters): Card[] =>
-  new Array(5).fill(null).map((_item, index) => {
-    if (parameters?.isOneIdLiteral && index > 0) {
-      parameters.id = '';
-    }
+const getRandomIndex = (min: number, max: number) =>
+  Math.round(Math.random() * (max - min) + min);
 
-    return {
-      id: parameters?.id ?? datatype.uuid(),
-      title: name.title(),
-      type: random.word(),
-      price: +commerce.price(),
-      city: {
-        name: address.cityName(),
-        location: {
+const makeFakeOffers = (parameters?: OffersParameters): Card[] => {
+  const arrayLength = 5;
+  const randomIndex = getRandomIndex(0, arrayLength);
+
+  return new Array(arrayLength).fill(null).map(
+    (_item, index) =>
+      ({
+        id:
+          parameters?.isOneIdLiteral && index === randomIndex
+            ? parameters?.id
+            : datatype.uuid(),
+        title: name.title(),
+        type: random.word(),
+        price: +commerce.price(),
+        city: {
+          name: address.cityName(),
+          location: {
+            latitude: +address.latitude(),
+            longitude: +address.longitude(),
+            zoom: datatype.number(20),
+          },
+        },
+        location: parameters?.location ?? {
           latitude: +address.latitude(),
           longitude: +address.longitude(),
           zoom: datatype.number(20),
         },
-      },
-      location: parameters?.location ?? {
-        latitude: +address.latitude(),
-        longitude: +address.longitude(),
-        zoom: datatype.number(20),
-      },
-      isFavorite: parameters?.isFavorite ?? datatype.boolean(),
-      isPremium: datatype.boolean(),
-      rating: datatype.number(5),
-      previewImage: system.filePath(),
-    } as Card;
-  });
+        isFavorite: parameters?.isFavorite ?? datatype.boolean(),
+        isPremium: datatype.boolean(),
+        rating: datatype.number(5),
+        previewImage: system.filePath(),
+      } as Card)
+  );
+};
 
 export { makeFakeOffers };
