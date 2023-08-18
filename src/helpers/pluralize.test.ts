a42d2c6226330236e4ca4f7e884394635124dbfa
NeXan <nexan87@gmail.com>
2023-08-18 12:25:53 +0300

    Пишет тест для функции pluralize

---
+++ src/helpers/pluralize.test.ts
@@ -0,0 +1,37 @@
+import { pluralize } from './pluralize';
+
+describe('Function: pluralize', () => {
+  const expectedWord = 'Bedroom';
+
+  it('If the quantity of elements is more than one, then the plural ending is displayed in the word', () => {
+    const quantity = 5;
+
+    const result = pluralize(expectedWord, quantity);
+
+    expect(result).toBe('Bedrooms');
+  });
+
+  it('If the quantity of elements is equal to one, then the plural ending is displayed in the word', () => {
+    const quantity = 1;
+
+    const result = pluralize(expectedWord, quantity);
+
+    expect(result).toBe('Bedroom');
+  });
+
+  it('Incorrect word output when the quantity of elements is more than one', () => {
+    const quantity = 2;
+
+    const result = pluralize(expectedWord, quantity);
+
+    expect(result).not.toBe('Bedroom');
+  });
+
+  it('Incorrect word output if the quantity of elements is equal to one', () => {
+    const quantity = 1;
+
+    const result = pluralize(expectedWord, quantity);
+
+    expect(result).not.toBe('Bedrooms');
+  });
+});
